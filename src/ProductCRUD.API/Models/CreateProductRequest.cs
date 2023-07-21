f7f438cf07d95bcb7d54bb9b0e664566b9d6d88c
SMK Consulting <smkconsulting@outlook.com>
2023-07-21 11:38:04 +0000

    Update on docker

---
+++ src/ProductCRUD.API/Models/CreateProductRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductCRUD.API.Models
+{
+    public class CreateProductRequest
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+        // public record CreateProductRequest(string Name, string Description, decimal Price);
+    }
+}
---
+++ src/ProductCRUD.API/ProductDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCRUD.API.Models;
+
+namespace ProductCRUD.API
+{
+    public class ProductDbContext: DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
